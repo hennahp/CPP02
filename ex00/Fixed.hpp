ac093d06ba45032e1e02191ddfeea15fe775780c
hennahp <hparveen777@gmail.com>
2025-11-07 10:22:27 +0400

    Fixed.hpp done

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hparveen <hparveen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 09:51:10 by hparveen          #+#    #+#             */
+/*   Updated: 2025/11/07 10:22:09 by hparveen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define RESET "\033[0m"
+
+class Fixed
+{
+    private:
+        int                 fixedPointValue;
+        static const int    fractionalBits = 8;
+
+    public:
+        Fixed();
+        Fixed(const Fixed &other);
+        Fixed &operator=(const Fixed &other);
+        ~Fixed();
+        
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+};
+#endif
